f0ce5b1b4737fb74503665d9366640c7c332170a
Elías Puma <elpumitaelias@gmail.com>
2013-06-14 09:23:21 -0500

    Parseo del XML por medio de DOM

--- WeatherForecastProject/WeatherForecast/src/main/java/csunsa/is1/weatherforecast/MainActivity.java
+++ WeatherForecastProject/WeatherForecast/src/main/java/csunsa/is1/weatherforecast/MainActivity.java
@@ -5,10 +5,21 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.widget.TextView;
 
+import java.net.URL;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+
 import csunsa.is1.weatherforecast.Info_Structures.single_reading;
 
 public class MainActivity extends Activity {
-    private static final String URL =
+    private static final String XML_URL =
             "http://api.openweathermap.org/data/2.5/weather?lat=-16.3691842&lon=-71.4985723&mode=xml&units=metric&lang=sp";
 
     public single_reading m_reading = new single_reading();
@@ -18,6 +29,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        get_reading();
         show_Reading();
     }
 
@@ -29,7 +41,7 @@ public class MainActivity extends Activity {
     }
 
     public void show_Reading () {
-        //*****************TEST**********************//
+        /*****************TEST**********************/
         String  city = "Arequipa",
                 country = "PE",
                 desc = "Descrip",
@@ -39,15 +51,30 @@ public class MainActivity extends Activity {
         m_reading.set_country_name(country);
         m_reading.set_descr(desc);
         m_reading.set_ico(ico);
-        //*******************************************//
+        /*******************************************/
 
         TextView _textField = (TextView) findViewById(R.id.city_field);                         //Objeto para cambiar todos los campos de texto
         _textField.setText(m_reading.get_city_name() + ", " + m_reading.get_country_name());    //Ciudad, País
 
         _textField = (TextView) findViewById(R.id.descr_field);                                 //Descripcion
         _textField.setText(m_reading.get_descr());
+    }
+
+    public void get_reading() {
+        try {
+
+            URL url = new URL(XML_URL);
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(new InputSource(url.openStream()));
+            doc.getDocumentElement().normalize();
+
+            NodeList nodeList = doc.getElementsByTagName("item");
 
 
+        } catch (Exception e) {
+            System.out.println("XML Parsing Exception = " + e);
+        }
     }
 }
 
